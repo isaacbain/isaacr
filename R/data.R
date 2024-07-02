86a25a5d2d81592f30b67b6e463b091af486d83e
Isaac Bain <isaacbain@gmail.com>
2024-07-03 10:44:11 +1200

    added leaflet maps to lcdb symbology vignette

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' LCDB5 data cropped to Christchurch
+#'
+#' A subset of Land Cover Database version 5 (LCDB5) data cropped to the Christchurch area.
+#'
+#' @format
+#' An SF data object with 2,232 features and 23 fields.
+#' NZTM2000 projection. CRS: 2193.
+#' Clipped to bbox: xmin = 1543016, xmax = 1587730, ymin = 5169087, ymax = 5190086 .
+#' @source <https://lris.scinfo.org.nz/layer/104400-lcdb-v50-land-cover-database-version-50-mainland-new-zealand/>
+"lcdb5_chch"
+
+#' LCDB5 data cropped to Blenheim
+#'
+#' A subset of Land Cover Database version 5 (LCDB5) data cropped to the Blenheim area.
+#'
+#' @format
+#' An SF data object with 2,232 features and 23 fields.
+#' NZTM2000 projection. CRS: 2193.
+#' Clipped to bbox: xmin = 1660092, xmax = 1693589, ymin = 5396530, ymax = 5412816 .
+#' @source <https://lris.scinfo.org.nz/layer/104400-lcdb-v50-land-cover-database-version-50-mainland-new-zealand/>
+"lcdb5_blenheim"
